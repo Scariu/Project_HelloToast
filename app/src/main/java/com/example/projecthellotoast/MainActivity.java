e20eb111245568885fe9e6a63492f7486d5bfc21
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-19 21:06:01 -0400

    Se agrega funcionalidad al botón Toast.

--- app/src/main/java/com/example/projecthellotoast/MainActivity.java
+++ app/src/main/java/com/example/projecthellotoast/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.projecthellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
     }
+
+    private void initListeners() {
+        Button botonToast = findViewById(R.id.buttonShowToast);
+        Button botonCounter = findViewById(R.id.buttonCount);
+        TextView texto = findViewById(R.id.textResultado);
+
+        botonToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hello Toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
 }
